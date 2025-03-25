8624816880149776b24dba9203f2e513903bc082
Manuel-agl <a920166742@my.uvm.edu.mx>
2025-03-25 16:59:01 -0600

    Subida de archivos2

---
+++ Script_Materias.sql
@@ -0,0 +1,8 @@
+##Tabla materias
+
+create table materias(
+	id_materia int primary key, #llave primaria de 
+	nombre_materia varchar(50), #nombre de la materia
+	area_conocimiento varchar(50), #are de conocimiento
+	descripcion varchar(50), #descripcion de la materia
+);
